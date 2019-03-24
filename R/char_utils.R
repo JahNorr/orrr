a5ffc0db509ce4b695627f8b6587a284e27ff1a7
John Orr <jah.Norr@gmail.com>
2019-03-24 07:54:54 -0400

    added string_ruler()

--- R/char_utils.R
+++ R/char_utils.R
@@ -66,3 +66,88 @@ spaces<-function(n) {
 }
 
 
+#' String ruler
+#' Fuction to assist with visualizing positions of a string, usually used with long fixed width
+#' character vectors
+#' @param x - character: string to measure
+#' @param split - integer: split lines every [split] characters
+#' @param under - logical: underline the ruler digits
+#' @param uchar - character: character to use for the underline (default="-")
+#' @param nl - integer: number of newlines after each ruled line
+#' @param nlchar - character: character to use for the new lines (default=" ")
+#'
+#' @return character: string with "ruler" above
+#' @export
+#'
+string_ruler<-function(x,split=0,under=T,uchar="-",nl=0,nlchar=" ") {
+
+  slen<-nchar(x[1])
+  #strret<-""
+  lines<-character(0)
+  sp9<-strrep(" ",9)
+
+  if(slen>999) {
+    s<-strrep(" ",999)
+    s_thou<-""
+    sapply(1:(slen%/%1000), function(i) {
+      s_thou<<-paste0(s_thou,s,i)
+    })
+    lines<-c(lines,s_thou)
+  }
+
+  if(slen>99) {
+
+    s_hun<-""
+    sapply(seq(10,slen,10), function(i) {
+      if(i<100) c<-" " else c<-as.character(i%/%100)
+      s_hun<<-paste0(s_hun,sp9,c)
+    })
+    lines<-c(lines,s_hun)
+  }
+
+  if(slen>9) {
+    s_dec<-""
+    sapply(1:(slen%/%10), function(i) {
+      s_dec<<-paste0(s_dec,sp9,i%%10)
+    })
+    lines<-c(lines,s_dec)
+  }
+
+  s<-strrep(" ",9)
+  s_dig<-""
+  sapply(1:slen, function(i) {
+    s_dig<<-paste0(s_dig,i%%10)
+  })
+
+  lines<-c(lines,s_dig)
+
+  if(under) lines<-c(lines,strrep(uchar,slen) )
+
+  lines<-c(lines,x)
+  if(nl>0) lines<-c(lines,rep(strrep(nlchar,slen),nl))
+
+  if(split>0) {
+    newlines<-character(0)
+    nsplits<-((slen-1)%/%split)+1
+
+    df<-data.frame(tmp=1:length(lines))
+    sapply(1:nsplits, function(col) {
+      start<-(col-1)*split+1
+      end<-start+split-1
+      addlines<-substr(lines,start,end)
+      newlines<<-c(newlines,addlines)
+      df[,col]<<-addlines
+      invisible()
+    })
+    cat(paste0(newlines,collapse = "\n"))
+
+  } else {
+    cat(paste0(lines,collapse = "\n"))
+
+  }
+
+
+  invisible()
+
+}
+
