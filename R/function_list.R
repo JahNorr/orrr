3b607fcd0efbd6460d9ee5ce9f8d8a1a71ada66a
John Orr <jah.Norr@gmail.com>
2021-10-03 11:51:10 -0600

    fixed - the list.functions to handle R6 opbjects - the get.rdata function to handle characters

--- R/function_list.R
+++ R/function_list.R
@@ -14,7 +14,38 @@
 #' @return invisible(data.frame) -  with columns for folder, file, and function
 #' @export
 #'
-list.functions<-function(files,saveto=NULL,verbose=T) {
+list.functions<-function(files,saveto=NULL,verbose=TRUE, sorted=TRUE) {
+
+  handle_funcs<-function(folder,file,funcs,sub=NULL,sorted=TRUE) {
+
+    if(sorted) funcs<-sort(funcs)
+
+    new_fldr<-fldr!=folder
+    new_file<-file!=file_old
+
+    if(new_fldr) {
+      cat("=================================================================================\n")
+      cat(folder,"\n")
+    }
+
+    if(new_file) {
+      if(!new_fldr) cat("---------------------------------------------------------\n")
+      cat("\t",file,"\n")
+    }
+
+
+    if(!is.null(sub))   {
+      cat(paste0("\t\t",sub,"\n"))
+    }
+
+    cat(paste0("\t\t\t",funcs,"\n"))
+
+    file_old<<-file
+    fldr<<-folder
+  }
+
+  ##    end of handle_funcs function
+  ###############################################################
   if(!is.null(saveto)) {
     sink(saveto)
     verbose<-TRUE
@@ -32,42 +63,84 @@ list.functions<-function(files,saveto=NULL,verbose=T) {
 
   df<-data.frame(stringsAsFactors = F)
   fldr<-""
+  file_old<-""
+
   mapply(function(folder,file){
     folder<-gsub("\\","/",normalizePath(folder),fixed=T)
     lines<-readLines(con = file)
 
+    isR6<-any(grepl("[<]- {0,}R6Class[(]",lines))
+
+    pvtR6<-0
+    pubR6<-0
+    actR6<-0
+
+    if(isR6) {
+      pvtR6<-grep("private {0,}= {0,}list[(]",lines)
+      pubR6<-grep("public {0,}= {0,}list[(]",lines)
+      actR6<-grep("active {0,}= {0,}list[(]",lines)
+
+      if (length(pvtR6)==0) pvtR6<-0 else lines[pvtR6] <- "................\n  private\n............"
+      if (length(pubR6)==0) pubR6<-0
+      if (length(actR6)==0) actR6<-0
+    }
+
     file<-gsub("(.*/)(.*[.R])","\\2",file)
 
-    func_lines<-grep("[<]- {0,}function[(]",lines)
+    func_lines<-grep("([<]-|=) {0,}function[(]",lines)
+    #   func_lines<-c(func_lines,pvtR6,pubR6,actR6)
+    func_lines<-func_lines[func_lines>0]
+    func_lines<-sort(func_lines)
 
     funcs<-lines[func_lines]
+    rm<-c(grep("#.*function",funcs),
+          grep("[ms]apply",funcs))
 
-    funcs<-funcs[!grepl("#.*function",funcs)]
+    if(length(rm)>0) {
+      funcs<-funcs[-rm]
+      func_lines<-func_lines[-rm]
+    }
+
     funcs<-gsub( " {0,}<- {0,}function","",funcs)
 
-    funcs<-funcs[!grepl("[ms]apply",funcs)]
+    # funcs<-funcs[!grepl("[ms]apply",funcs)]
+    # funcs<-funcs[!grepl("#.*function",funcs)]
 
     funcs<-stringr::str_trim(gsub( " {0,}[{]","",funcs))
-    funcs<-sort(funcs)
 
-    if(length(funcs)>0) {
-      df<<-rbind(df,data.frame(folder=folder,file=file,func=funcs))
-      new_fldr<-fldr!=folder
-      if(new_fldr) {
-        if(verbose) {
-          cat("=================================================================================\n")
-          cat(folder,"\n")
-        }
-        fldr<<-folder
-      }
-      if(verbose) {
-        if(!new_fldr) cat("---------------------------------------------------------\n")
+    if(isR6) {
+      begin<-c(pvtR6,pubR6,actR6)
+      type<-c("private","public","active")
 
-        cat("\t",file,"\n")
-        cat(paste0("\t\t",funcs,"\n"))
+      kp<-begin>0
 
+      begin<-begin[kp]
+      type<-type[kp]
+
+      ord<-order(begin)
+      begin<-begin[ord]
+      type<-type[ord]
+      ntypes<-length(type)
+
+      if(ntypes>1) end<-begin[2:length(begin)] else end<-integer(0)
+      end<-c(end,length(lines))
+
+      mapply(function(f0,f1,typ) {
+        fls<-which(func_lines>f0 & func_lines<f1)
+        funs<-funcs[fls]
+        df<<-rbind(df,data.frame(folder=folder,file=typ,func=funs))
+        handle_funcs(folder = folder,file = file,funcs = funs,sub=typ, sorted = sorted)
+      },begin,end,type)
+
+
+    } else {
+      if(length(funcs)>0) {
+        df<<-rbind(df,data.frame(folder=folder,file=file,func=funcs))
+        handle_funcs(folder = folder,file = file,funcs = funcs,sorted = sorted)
       }
     }
+
+
   },folders,files)
 
   if(!is.null(saveto)) sink(NULL)
--- R/list_rdata.R
+++ R/list_rdata.R
@@ -24,17 +24,34 @@ list.rdata<-function(file){
 #'  Returns the nth item in a data file
 #'
 #' @param file - character name of file
-#' @param item - integer item to retrieve (default=1)
-#' @return nth item in a .RData file
+#' @param item - integer item to retrieve (default=1) or character matching one item (using grep)
+#' @return requested item in a .RData file
 #' @export
 #'
 #' @examples
 #' dir.project()
 #'
 
+
 get.rdata<-function(file,item=1){
   e<-new.env()
 
   load(file = file,envir = e)
+  if(is.character(item)) {
+    match<-grep(item,ls(e))
+    nmatches<-length(match)
+    if(nmatches>0) {
+      item<-match[1]
+      if(nmatches>1) warning("There was more than 1 match, using the first one")
+    } else {
+      match<-0
+      warning("There were no matches.")
+      return(NULL)
+    }
+  }
+
   get(ls(e)[item],envir = e)
 }
+
+
+
